32a70d1a682dfc17e4a3ddc22940af73cb321962
Laura Pana <laurapana@Lauras-MacBook-Pro.local>
2022-04-17 18:44:11 +0100

    Installed react-native-vector-icons

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -2,11 +2,13 @@ import {StyleSheet, Text, View, TextInput} from 'react-native';
 import React from 'react';
 import {FONT_XLARGE} from '../../constants/LAYOUT';
 import {BLUE} from '../../constants/COLORS';
+import TemplateIcon from '../../components/TemplateIcon';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <TextInput style={styles.search}>Search</TextInput>
+      <TemplateIcon name="search" size={50} />
     </View>
   );
 };

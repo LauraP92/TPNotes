62548d8094f66184f0af6b20444f0a15a6e695ee
Laura Pana <laurapana@Lauras-MacBook-Pro.local>
2022-04-17 16:20:26 +0100

    Init project

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import HomeScreen from './src/screens/Home/HomeScreen';
+
+const App: () => Node = () => {
+  return <HomeScreen />;
+};
+
+export default App;
